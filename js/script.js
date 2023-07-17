2eed66d1669e0584b3a85628a2201e1d83a54b59
alecoad <andrewlecoad@gmail.com>
2023-07-16 21:45:00 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// GLOBAL VARIABLES
+
+// The unordered list where the player’s guessed letters will appear.
+const guessedLetters = document.querySelector(".guessed-letters");
+// The button with the text “Guess!” in it.
+const guessButton = document.querySelector(".guess");
+// The text input where the player will guess a letter.
+const letterGuess = document.querySelector(".letter");
+// The empty paragraph where the word in progress will appear.
+const wordInProgress = document.querySelector(".word-in-progress");
+// The paragraph where the remaining guesses will display.
+const remainingGuesses = document.querySelector(".remaining");
+// The span inside the paragraph where the remaining guesses will display.
+const remainingGuessesSpan = document.querySelector("span");
+// The empty paragraph where messages will appear when the player guesses a letter.
+const message = document.querySelector(".message");
+// The hidden button that will appear prompting the player to play again.
+const playAgainButton = document.querySelector("play-again");
+
+const word = "magnolia";
+
+// FUNCTION TO ADD PLACEHOLDERS FOR EACH LETTER
